cdcbf8e7bcb552b5a61d9cfd0c12f3b8a207134f
SumaJillela777 <sxj50960@ucmo.edu>
2023-06-15 21:06:49 -0500

    initial commit

---
+++ AzureProject/Controllers/EnrollmentController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AzureProject.Models;
+
+namespace AzureProject.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        private readonly AzureContext _context;
+
+        public EnrollmentController(AzureContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Enrollment
+        public async Task<IActionResult> Index()
+        {
+            var azureContext = _context.Enrollments.Include(e => e.Course);
+            return View(await azureContext.ToListAsync());
+        }
+
+        // GET: Enrollment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Enrollments == null)
+            {
+                return NotFound();
+            }
+
+            var enrollment = await _context.Enrollments
+                .Include(e => e.Course)
+                .FirstOrDefaultAsync(m => m.EnrollmentID == id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseID"] = new SelectList(_context.Courses, "CourseID", "CourseID");
+            return View();
+        }
+
+        // POST: Enrollment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EnrollmentID,CourseID,StudentID,Grade")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(enrollment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseID"] = new SelectList(_context.Courses, "CourseID", "CourseID", enrollment.CourseID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Enrollments == null)
+            {
+                return NotFound();
+            }
+
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseID"] = new SelectList(_context.Courses, "CourseID", "CourseID", enrollment.CourseID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EnrollmentID,CourseID,StudentID,Grade")] Enrollment enrollment)
+        {
+            if (id != enrollment.EnrollmentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(enrollment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EnrollmentExists(enrollment.EnrollmentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseID"] = new SelectList(_context.Courses, "CourseID", "CourseID", enrollment.CourseID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Enrollments == null)
+            {
+                return NotFound();
+            }
+
+            var enrollment = await _context.Enrollments
+                .Include(e => e.Course)
+                .FirstOrDefaultAsync(m => m.EnrollmentID == id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Enrollments == null)
+            {
+                return Problem("Entity set 'AzureContext.Enrollments'  is null.");
+            }
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment != null)
+            {
+                _context.Enrollments.Remove(enrollment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EnrollmentExists(int id)
+        {
+          return (_context.Enrollments?.Any(e => e.EnrollmentID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AzureProject/Models/AzureContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureProject.Models
+{
+    public class AzureContext : DbContext
+    {
+        public AzureContext(DbContextOptions<AzureContext> options) : base(options) { }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
